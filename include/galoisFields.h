7ae15f9ddf3a4d09df66b2e614c1188e085cf1f3
Ignacio Ribas <igrib98@gmail.com>
2021-05-11 13:58:21 -0300

    [Galois Fields] Sum and multiplication mod generating polynomial implemented

---
+++ include/galoisFields.h
@@ -0,0 +1,7 @@
+#ifndef __GALOIS_FIELDS__
+#define __GALOIS_FIELDS__
+
+uint16_t sumPolynomials(uint16_t p1, uint16_t p2);
+uint16_t multiplyPolynomials (uint16_t p1, uint16_t p2);
+
+#endif
--- src/galoisFields.c
+++ src/galoisFields.c
@@ -1,9 +1,75 @@
 #include <stdint.h>
+#include "../include/galoisFields.h"
+#include <stdio.h>
+#include <malloc.h>
 
-const uint16_t generatingPolynomial = 0x0163;
+static const uint16_t generatingPolynomial = 0x0163;
+static const uint8_t WORD_BIT_LENGTH = 16;
+
 
 // For tidiness purposes only
 uint16_t sumPolynomials(uint16_t p1, uint16_t p2) {
     return p1^p2;
 }
 
+// Find degree (aka index of most significant bit set to 1)
+static uint8_t findDegree(uint16_t p) {
+    int d;
+    for (d = 0; p != 0; d++) {
+        p = p >> 1;
+    }
+    return (d==16) ? d-2: d-1;
+}
+
+uint16_t multiplyModGenP (uint16_t p1, uint16_t p2) {
+
+    // Multiplication part
+    
+    // find bits set to 1; first find largest polynomial
+    uint16_t sP, lP; 
+    if (p1 > p2) {
+        lP = p1; sP = p2;
+    } else {
+        lP = p2; sP = p1;
+    }
+
+    uint16_t ithBit;
+    uint16_t xorValue = 0x0000;
+    for (uint8_t i = 0; i < WORD_BIT_LENGTH; i++) {
+        ithBit = (sP >> i) << (WORD_BIT_LENGTH -1);
+        ithBit = ithBit >> (WORD_BIT_LENGTH -1);
+        switch (ithBit) {
+            case 0:
+                break;
+            case 1:
+                xorValue = xorValue ^ (lP<<i);
+                break;
+            default:
+                printf("Error multiplying - shifting bits from largest polynomial");
+                exit(1);
+                //break;
+        }
+    }
+
+    // Reduction part
+
+    uint16_t rest = xorValue;
+    uint16_t genPDegree = findDegree(generatingPolynomial);
+    uint16_t restDegree = findDegree(rest);
+    while (restDegree - genPDegree >= 0) {
+        rest = rest ^ (generatingPolynomial << (restDegree-genPDegree));
+        restDegree = findDegree(rest);
+    }
+
+    return rest;
+
+}
+
+int main (int argc, char * argv[]) {
+    uint16_t x = 0x0163;
+    uint16_t y;
+
+    multiplyModGenP(0x0054, 0x001D);
+    
+    return 0;
+}
