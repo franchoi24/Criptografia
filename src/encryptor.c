1582fea27d959dc4e0b79c76be98e38b5c4daca7
Ignacio Ribas <igrib98@gmail.com>
2021-05-13 09:10:38 -0300

    Modifying a single shade image, halts at ~12% for some reason

--- src/encryptor.c
+++ src/encryptor.c
@@ -22,6 +22,14 @@ static void turnRowsUpsideDown(uint8_t * bitmapData, BITMAPINFOHEADER infoHeader
     }
 }
 
+// Bit twiddling hacks, stackoverflow
+
+char calcParityBit (unsigned char v)
+{
+    return (0x6996u >> ((v ^ (v >> 4)) & 0xf)) & 1;
+}
+
+
 int main (int argc, char *argv[]) {
     BITMAPINFOHEADER secretBmpIH = {0};
     uint8_t *secretBitmapData;
@@ -42,24 +50,52 @@ int main (int argc, char *argv[]) {
         // calculate number of 2x2 blocks that fit horizontal line in shade image
         int shadeXBlockNo = shadeBmpIH.biWidth / 2;
         uint8_t * currentBlockStart = secretBitmapData;
-        for (int currentBlockNo = 0; currentBlockNo < secretBlockCount; ++currentBlockNo, currentBlockStart += k) {
+
+        int Xind, Uind, Vind, Wind;
+        uint8_t FxByte;
+
+        uint8_t newW;
+        uint8_t newV;
+        uint8_t newU;
+        uint16_t F_X;
+
+        for (int currentBlockNo = 0; currentBlockNo < secretBlockCount; ++currentBlockNo) {
             // for each blockNo, get XUVW. Then just calculate F(X) and replace where necessary
-            int Xind, Uind, Vind, Wind;
-            // just do the math
+            // just do the math, maybe it gets easier to compute with value of the prev iteration
             Xind =  (currentBlockNo / shadeXBlockNo) * 2 * shadeBmpIH.biWidth + (currentBlockNo % shadeXBlockNo) * 2;
             Vind = ((currentBlockNo / shadeXBlockNo) * 2 + 1) * shadeBmpIH.biWidth + (currentBlockNo % shadeXBlockNo) * 2;
             Uind = Vind + 1;
             Wind = Xind + 1;
 
             // Now, evaluate polynomial in Xind
-            uint16_t F_X = 0x0;
+            F_X = 0x0;
             for (int pow = 0; pow < k; pow++) {
-                F_X = sumPolynomials(F_X, multiplyModGenP(currentBlockStart[pow], galoisPower(shadeBitmapData[Xind], pow) ) );
+                F_X ^= multiplyModGenP(currentBlockStart[pow], galoisPower(shadeBitmapData[Xind], pow) );
             }
-
-            // Having F_X, change values for U, V and W
             
+            FxByte = (uint8_t) F_X;
+        
+            // Having F_X, change values for U, V and W
+
+            // first calculate them here for clarity
+            newW = (shadeBitmapData[Wind] & 0b11111000) | ((FxByte & 0b11100000)>>5); // Three highest bits of FX in 3 lowest bits of ~W
+            newV = (shadeBitmapData[Vind] & 0b11111000) | ((FxByte & 0b00011100)>>2); // Second group of 3 highest bits of FX in 3 lower bits of ~V
+            newU = (shadeBitmapData[Uind] & 0b11111000) | (FxByte & 0b00000011) | (calcParityBit(FxByte) << 2); // 2 LB of FX in 2 LB of ~U but also parity in 3rd lowest bit of ~U
+        
+            // then write them off
+            shadeBitmapData[Wind] = newW;
+            shadeBitmapData[Vind] = newV;
+            shadeBitmapData[Uind] = newU;
+            currentBlockStart += k;
+            printf("Progress: %g\n", currentBlockNo/(double)secretBlockCount);
         }
+
+        // save the image to see if it works
+        turnRowsUpsideDown(shadeBitmapData, shadeBmpIH);
+
+        FILE * fd = fopen("res/newImg.bmp", "w");
+        fwrite(&shadeBmpIH, sizeof(shadeBmpIH), 1, fd);
+        fwrite(shadeBitmapData, shadeBmpIH.biSizeImage, 1, fd);
 
     return 0;
 }
